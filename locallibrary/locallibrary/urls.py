73e8f55f31175cf32115d1bd91576cfe2215f669
loganknight <loganknight16@rogers.com>
2024-04-03 22:01:31 -0500

    Finished part 5 and added the templates and css files

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -1,7 +1,8 @@
 """
 URL configuration for locallibrary project.
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,6 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Use include() to add URLS from the catalog application and authentication system
+from django.urls import include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
